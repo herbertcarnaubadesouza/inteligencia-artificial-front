d6beeca8ec770cfc3ca97192c0c28a1b19184a86
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2023-05-20 01:31:47 -0300

    progress

--- src/components/Home/LeftSide/index.tsx
+++ src/components/Home/LeftSide/index.tsx
@@ -1,8 +1,21 @@
+import './leftside.scss';
+
 function LeftSide() {
   return (
     <>
-      <div>
+      <div className="left-side-container">
         <img src="./images/logo.svg"></img>
+        <div className="progressBar-container">
+          <div className="progress"></div>
+          <div className="icons-container">
+            <img src="./images/bag.svg"></img>
+            <img src="./images/constructing.svg"></img>
+            <img src="./images/location.svg"></img>
+            <img src="./images/etiqueta.svg"></img>
+            <img src="./images/diamond.svg"></img>
+            <img src="./images/email.svg"></img>
+          </div>
+        </div>
       </div>
     </>
   );
