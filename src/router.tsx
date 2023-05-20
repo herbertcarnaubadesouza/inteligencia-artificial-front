31595b58e57d6afd6473607f29cf63960887fd40
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2023-05-20 00:33:07 -0300

    first commit

---
+++ src/router.tsx
@@ -0,0 +1,12 @@
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
+const AppRouter = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </Router>
+);
+
+export default AppRouter;
