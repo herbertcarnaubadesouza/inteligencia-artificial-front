e0a516ab3cb235a088a2b07d2492b51f2accb70f
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2023-06-03 11:42:38 -0300

    one more section

--- src/components/Templates/LeftSide/index.tsx
+++ src/components/Templates/LeftSide/index.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import './leftside.scss';
+import { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
-import { PaletteOne, PaletteTwo, PaletteTree, PaletteFour, FontOne, FontTwo, FontThree, FontFour } from '../Theme';
+import { FontOne, FontThree, FontTwo, PaletteOne } from '../Theme';
+import './leftside.scss';
 
-import Template01 from '../Template01'
-import Template02 from '../Template02'
-import Template03 from '../Template03'
+import Template01 from '../Template01';
+import Template02 from '../Template02';
+import Template03 from '../Template03';
 
 export function useLeftSide() {
   const [isVisible01, setIsVisible01] = useState(true);
@@ -38,18 +38,14 @@ export function useLeftSide() {
     setCurrentFont(FontThree);
   };
 
+  console.log(currentPalette);
+
   const jsx = (
     <ThemeProvider theme={{ ...currentPalette, font: currentFont }}>
       <div className="template-side-container">
-        {isVisible01 && (
-          <Template01 isVisible01={isVisible01} />
-        )}
-        {isVisible02 && (
-          <Template02 isVisible02={isVisible02} />
-        )}
-        {isVisible03 && (
-          <Template03 isVisible03={isVisible03} />
-        )}
+        {isVisible01 && <Template01 isVisible01={isVisible01} />}
+        {isVisible02 && <Template02 isVisible02={isVisible02} />}
+        {isVisible03 && <Template03 isVisible03={isVisible03} />}
       </div>
     </ThemeProvider>
   );
@@ -63,6 +59,6 @@ export function useLeftSide() {
     isVisible02,
     isVisible03,
     setCurrentPalette,
-    setCurrentFont
+    setCurrentFont,
   };
 }
--- src/components/Templates/Template01/styles.tsx
+++ src/components/Templates/Template01/styles.tsx
@@ -13,7 +13,7 @@ interface ThemeProps {
     fontBodyColor: string;
     fontTitleColor: string;
     font: FontProps;
-  }
+  };
 }
 
 const Container = styled.div`
@@ -28,14 +28,11 @@ const Container = styled.div`
     color: ${(props: ThemeProps) => props.theme.fontTitleColor};
     font-family: ${(props: ThemeProps) => props.theme.font.fontFamily};
     z-index: 999;
-
-
   }
 
   p {
     color: ${(props: ThemeProps) => props.theme.fontBodyColor};
     font-family: ${(props: ThemeProps) => props.theme.font.fontFamily};
-
   }
 `;
 
@@ -57,14 +54,15 @@ const HeaderFooter = styled.div`
   text-align: center;
 
   &::before {
-    content: "";
+    content: '';
     position: absolute;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
-    opacity: 30%;
-    background-color: ${(props: ThemeProps) => props.theme.headerFooterBgColor};  }
+    opacity: 20%;
+    background-color: ${(props: ThemeProps) => props.theme.headerFooterBgColor};
+  }
 
   h1 {
     font-size: 48px;
@@ -76,10 +74,7 @@ const HeaderFooter = styled.div`
     color: ${(props: ThemeProps) => props.theme.headerFooterTextColor};
     font-family: ${(props: ThemeProps) => props.theme.font.fontFamily};
     z-index: 999;
-
-
   }
-
 
   button {
     background-color: ${(props: ThemeProps) => props.theme.fontTitleColor};
@@ -94,7 +89,7 @@ const HeaderFooter = styled.div`
     margin-top: 2rem;
     cursor: pointer;
     z-index: 999;
-
+    border: none;
   }
 `;
 
@@ -113,13 +108,81 @@ const Navigation = styled.div`
   }
 `;
 
-const Main = styled.div`
-  display: flex; 
+const AboutSection = styled.section`
+  display: flex;
   flex-direction: row;
   padding-left: 12rem;
   padding-right: 12rem;
   padding-top: 2.5rem;
   padding-bottom: 4rem;
+`;
+
+const Parallax = styled.section`
+  padding-block: 10rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-image: url('./images/template01/parallax.jpg');
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  width: 100%;
+  position: relative;
+
+  h2 {
+    font-size: 40px;
+    max-width: 60rem;
+    color: ${(props: ThemeProps) => props.theme.headerFooterTextColor};
+    font-family: ${(props: ThemeProps) => props.theme.font.fontFamily};
+    z-index: 999;
+    margin-top: 0;
+  }
+  h4 {
+    color: ${(props: ThemeProps) => props.theme.headerFooterTextColor};
+    font-family: ${(props: ThemeProps) => props.theme.font.fontFamily};
+    z-index: 999;
+    font-size: 13px;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 10%;
+    background-color: ${(props: ThemeProps) => props.theme.headerFooterBgColor};
+  }
+`;
+
+const ParallaxContent = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  a {
+    background-color: #fff;
+    font-size: 20px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 74px;
+    height: 74px;
+
+    svg {
+      fill: ${(props: ThemeProps) => props.theme.fontTitleColor};
+    }
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
 `;
 
 const LogoTemplate = styled.img`
@@ -133,7 +196,16 @@ const LogoTemplate = styled.img`
 const Line = styled.div`
   height: 5px;
   width: 8rem;
-  background-color: ${(props: ThemeProps) => props.theme.fontTitleColor};  }
+  background-color: ${(props: ThemeProps) => props.theme.fontTitleColor};
+  border: none;
+  margin-bottom: 1rem;
+  z-index: 999;
+`;
+
+const LineParallax = styled.div`
+  height: 2px;
+  width: 4rem;
+  background-color: ${(props: ThemeProps) => props.theme.fontTitleColor};
   border: none;
   margin-bottom: 1rem;
   z-index: 999;
@@ -141,6 +213,17 @@ const Line = styled.div`
 
 const Info = styled.div`
   display: flex;
-`
+`;
 
-export { Container, HeaderFooter, Navigation, Main, LogoTemplate, Line, Info };
+export {
+  Container,
+  HeaderFooter,
+  Navigation,
+  AboutSection,
+  LogoTemplate,
+  Line,
+  LineParallax,
+  Parallax,
+  ParallaxContent,
+  Info,
+};
