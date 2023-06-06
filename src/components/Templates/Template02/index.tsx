1b19fb7871cf14bd3591e5bccba63696af230a56
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2023-06-06 17:39:05 -0300

    ajusting second templat

--- src/components/Templates/Template02/index.tsx
+++ src/components/Templates/Template02/index.tsx
@@ -1,6 +1,32 @@
 // index.tsx
-import { MapPin, PhoneCall } from '@phosphor-icons/react';
-import { Container, HeaderBlock, LineHeader, LineHeaderBottom } from './styles';
+import {
+  ArrowRight,
+  Baby,
+  FlagCheckered,
+  Handshake,
+  Heart,
+  LinkSimpleBreak,
+  MapPin,
+  NotePencil,
+  PhoneCall,
+  Scales,
+  Scroll,
+  User,
+} from '@phosphor-icons/react';
+import {
+  AboutSection,
+  Column,
+  Container,
+  FirstBlockAbout,
+  HeaderBlock,
+  LineAbout,
+  LineAboutSection,
+  LineHeader,
+  LineHeaderBottom,
+  Row,
+  SecondBlockAbout,
+  ServicesSection,
+} from './styles';
 import './template01.scss';
 
 interface Template02Props {
@@ -37,6 +63,113 @@ function Template02({ isVisible02 }: Template02Props) {
           </div>
         </div>
       </HeaderBlock>
+      <AboutSection>
+        <FirstBlockAbout>
+          <div className="container-block-about">
+            <LineAbout></LineAbout>
+            <span>Comprometidos em ajudar nossos clientes a ter sucesso</span>
+          </div>
+          <h2>Você veio ao lugar certo</h2>
+          <p>
+            Um parágrafo descritivo que diz aos clientes como você é bom e prova
+            que você é a melhor escolha que eles fizeram. Este parágrafo também
+            é para aqueles que procuram um advogado confiável. Você pode usar
+            algumas palavras atraentes e exibir suas capacidades que irão atrair
+            futuros clientes e incentivá-los a contratá-lo imediatamente. Liste
+            seus conhecimentos e experiência para provar que você é o melhor.
+          </p>
+          <div className="bottom-about-section">
+            <div className="block-bottom">
+              <LineAboutSection />
+              <Scales size={32} weight="fill" />
+              <h3>5 Anos de Experiência</h3>
+              <p>Officia officiis vitae hendrerit interdum pretium.</p>
+            </div>
+            <div className="block-bottom">
+              <LineAboutSection />
+              <FlagCheckered size={32} weight="fill" />
+              <h3>Definindo o sucesso</h3>
+              <p>Officia officiis vitae hendrerit interdum pretium.</p>
+            </div>
+          </div>
+        </FirstBlockAbout>
+        <SecondBlockAbout>
+          <img src="./images/template02/about.jpg"></img>
+        </SecondBlockAbout>
+      </AboutSection>
+      <ServicesSection>
+        <div className="full-content-website">
+          <h2>Nossas Áreas de Atuação</h2>
+          <Column>
+            <Row>
+              <div className="content-block-atuacao">
+                <LinkSimpleBreak size={32} weight="bold" />
+                <h3>Divórcio e Separação</h3>
+                <text>
+                  This is a short description elaborating the service you have
+                  mentioned above.​
+                </text>
+              </div>
+              <div className="content-block-atuacao">
+                <NotePencil size={32} weight="bold" />
+                <h3>Direitos dos avós</h3>
+                <text>
+                  This is a short description elaborating the service you have
+                  mentioned above.​
+                </text>
+              </div>
+              <div className="content-block-atuacao">
+                <Handshake size={32} weight="bold" />
+                <h3>Custódia de criança</h3>
+                <text>
+                  This is a short description elaborating the service you have
+                  mentioned above.​
+                </text>
+              </div>
+              <div className="content-block-atuacao">
+                <Baby size={32} weight="bold" />
+                <h3>Domestic Violence​​</h3>
+                <text>
+                  This is a short description elaborating the service you have
+                  mentioned above.​
+                </text>
+              </div>
+            </Row>
+            <Row>
+              <div className="content-block-atuacao">
+                <Heart size={32} weight="bold" />
+                <h3>Casamento/Uniões Civis​</h3>
+                <text>
+                  This is a short description elaborating the service you have
+                  mentioned above.​
+                </text>
+              </div>
+              <div className="content-block-atuacao">
+                <User size={32} weight="bold" />
+                <h3>Adoção e barriga de aluguel​</h3>
+                <text>
+                  This is a short description elaborating the service you have
+                  mentioned above.​
+                </text>
+              </div>
+              <div className="content-block-atuacao">
+                <Scroll size={32} weight="bold" />
+                <h3>Acordos de propriedade​​​</h3>
+                <text>
+                  This is a short description elaborating the service you have
+                  mentioned above.​
+                </text>
+              </div>
+              <div className="content-block-atuacao">
+                <div className="last-block-atuacao">
+                  <p>Ver Todos </p>
+                  <ArrowRight size={16} weight="bold" />
+                </div>
+              </div>
+            </Row>
+          </Column>
+        </div>
+      </ServicesSection>
     </Container>
   );
 }
--- src/components/Templates/Template02/styles.tsx
+++ src/components/Templates/Template02/styles.tsx
@@ -37,15 +37,35 @@ export const Container = styled.div`
     margin-right: 15%;
   }
   h2 {
-    color: ${(props: ThemeProps) => props.theme.fontTitleColor};
+    color: ${(props: ThemeProps) =>
+      mix(0.8, '#000', props.theme.headerFooterBgColor)};
     font-family: ${(props: ThemeProps) => props.theme.font.fontFamily};
     z-index: 999;
+    font-size: 2.5em;
+    margin-block: 1rem;
+    font-weight: bold;
+  }
+
+  h3 {
+    color: ${(props: ThemeProps) =>
+      mix(0.8, '#000', props.theme.headerFooterBgColor)};
+    font-family: ${(props: ThemeProps) => props.theme.font.fontFamily};
+    z-index: 999;
+    font-size: 1.2em;
+    margin-block: 1rem;
+    font-weight: bold;
+    margin: 0;
   }
   span {
     color: ${(props: ThemeProps) => props.theme.fontTitleColor};
     font-family: ${(props: ThemeProps) => props.theme.font.fontFamily};
     z-index: 999;
     font-weight: inherit;
+    font-size: 16px;
+  }
+
+  p {
+    margin: 0;
   }
 
   .container-block-header {
@@ -54,7 +74,7 @@ export const Container = styled.div`
     width: 100%;
     align-items: baseline;
     gap: 20px;
-    margin-left: -6rem;
+    position: relative;
   }
 `;
 
@@ -70,7 +90,7 @@ export const HeaderBlock = styled.section`
   background-size: cover;
   padding-top: 12rem;
   padding-bottom: 2rem;
-  padding-inline: 15rem;
+  padding-inline: 6rem;
   text-align: center;
 
   &::before {
@@ -118,19 +138,20 @@ export const HeaderBlock = styled.section`
 
   @media only screen and (min-width: 1620px) {
     background-size: cover;
-    /* width: 100%; */
     object-fit: cover;
     background-position: center center;
   }
 `;
 
 export const LineHeader = styled.div`
-  height: 1px;
-  width: 2.5rem;
+  height: 1.1px;
+  width: 3rem;
   background-color: ${(props: ThemeProps) => props.theme.fontTitleColor};
   border: none;
-  margin-bottom: 1rem;
   z-index: 999;
+  position: absolute;
+  left: -60px;
+  bottom: 8px;
 `;
 
 export const LineHeaderBottom = styled.div`
@@ -140,4 +161,141 @@ export const LineHeaderBottom = styled.div`
   border: none;
   margin-bottom: 1rem;
   z-index: 999;
+`;
+export const LineAboutSection = styled.div`
+  height: 1px;
+  width: 85%;
+  background-color: #a7a7a73e;
+  border: none;
+  z-index: 999;
+  margin-top: 1.5rem;
+  margin-bottom: 10px;
+`;
+
+export const AboutSection = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-inline: 6rem;
+  padding-block: 5rem;
+
+  .container-block-about {
+    display: flex;
+    align-items: baseline;
+    position: relative;
+  }
+`;
+export const LineAbout = styled.div`
+  height: 1.1px;
+  width: 3rem;
+  background-color: ${(props: ThemeProps) => props.theme.fontTitleColor};
+  border: none;
+  z-index: 999;
+  position: absolute;
+  left: -60px;
+  bottom: 8px;
+`;
+
+export const FirstBlockAbout = styled.div`
+  width: 50%;
+
+  .bottom-about-section {
+    display: flex;
+    justify-content: space-between;
+
+    .block-bottom {
+      display: flex;
+      flex-direction: column;
+      gap: 0.6rem;
+    }
+
+    svg {
+      fill: ${(props: ThemeProps) => props.theme.fontTitleColor};
+    }
+  }
+`;
+export const SecondBlockAbout = styled.div`
+  position: relative;
+  img {
+    width: 420px;
+    height: 485px;
+    object-fit: cover;
+    position: absolute;
+    bottom: 50px;
+  }
+  background-color: transparent;
+  background-image: linear-gradient(
+    90deg,
+    rgba(177, 151, 107, 0) 50%,
+    rgba(177, 151, 107, 0.08) 50%
+  );
+
+  width: 520px;
+  height: 585px;
+`;
+
+/*SERVICES*/
+
+export const ServicesSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+
+  background-color: ${(props: ThemeProps) =>
+    mix(0.99, '#fcfcfc', props.theme.headerFooterBgColor)};
+
+  svg {
+    fill: ${(props: ThemeProps) => props.theme.fontTitleColor};
+  }
+
+  h2 {
+    text-align: center;
+  }
+
+  .full-content-website {
+    padding-inline: 6rem;
+    padding-block: 5rem;
+    margin-inline: auto;
+  }
+`;
+
+export const Row = styled.div`
+  display: flex;
+  align-items: center;
+
+  .content-block-atuacao {
+    border: 1px solid #e1e1e186;
+    display: flex;
+    flex-direction: column;
+    gap: 11px;
+    justify-content: center;
+    padding-inline: 30px;
+    width: 248px;
+    height: 222px;
+
+    .last-block-atuacao {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 16px;
+      text-align: center;
+      text-transform: uppercase;
+
+      svg {
+        fill: #000;
+      }
+    }
+
+    &:hover {
+      background-color: #fff;
+      cursor: pointer;
+
+      .last-block-atuacao {
+        color: ${(props: ThemeProps) => props.theme.fontTitleColor};
+      }
+    }
+  }
+`;
+export const Column = styled.div`
+  margin-top: 2rem;
 `;
