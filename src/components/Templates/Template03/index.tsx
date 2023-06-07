0fa2de6dce1e47d681ad4508d6fa48ff21dd9b68
alexherivelton <alexherivelton@gmail.com>
2023-06-07 17:28:09 -0300

    Projeto03

--- src/components/Templates/Template03/index.tsx
+++ src/components/Templates/Template03/index.tsx
@@ -1,13 +1,25 @@
 // index.tsx
+
+import FlagCheckered from '@phosphor-icons/react/dist/icons/FlagCheckered';
 import {
   AboutSection,
+  AwardsRow1,
+  AwardsSection,
   Container,
+  HeaderAwards,
   HeaderBlock,
+  HowWorksHeaderSection,
+  HowWorksSection,
   IconSection,
+  LeftAwardBlock,
   LeftSideBlock,
   LineAboutSection,
+  LineAwardsSection,
+  PraticaceSection,
   RightSideBlock,
+  RigthAwardBlock,
 } from './styles';
+import { BagSimple, Buildings, Hammer, Scales } from '@phosphor-icons/react';
 
 interface Template03Props {
   isVisible03: boolean;
@@ -67,8 +79,161 @@ function Template03({ isVisible03 }: Template03Props) {
         </RightSideBlock>
       </AboutSection>
       <IconSection>
-        <h2>teste</h2>
+        <div className='Container-blocks'>
+          <div className="meta-icons">
+          <Scales size={50} weight="fill" />
+          <h3>120+</h3>
+          <p>Casos Resolvidos</p>
+          </div>
+
+          <div className="meta-icons">
+          <Hammer size={50} weight="fill" />
+          <h3>120+</h3>
+          <p>Clientes Satisfeitos</p>
+          </div>
+
+          <div className="meta-icons">
+          <Buildings size={50} weight="fill" />
+          <h3>120+</h3>
+          <p>Escritorios</p>
+          </div>
+
+          <div className="meta-icons">
+          <BagSimple size={50} weight="fill" />
+          <h3>120+</h3>
+          <p>Associados Legais</p>
+          </div>
+        </div>
       </IconSection>
+      <AwardsSection>
+      <LineAwardsSection></LineAwardsSection>
+        <HeaderAwards>
+          <div className='header-awards'>
+          <h2>Prêmios e reconhecimentos</h2>
+          <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque.</p>
+        </div>
+        </HeaderAwards>
+
+        <AwardsRow1>
+
+        <div className='awards-block'>
+        <img src="./images/template03/award5.png" alt="" />
+        <h3>Prêmio de Melhor Escritório de Advocacia</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        </div>
+
+        <div className='awards-block'>
+        <img src="./images/template03/award5.png" alt="" />
+        <h3>Prêmio de Melhor Escritório de Advocacia</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        </div>
+
+        <div className='awards-block'>
+        <img src="./images/template03/award5.png" alt="" />
+        <h3>Prêmio de Melhor Escritório de Advocacia</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        </div>
+
+        </AwardsRow1>
+
+        <AwardsRow1>
+
+        <div className='awards-block'>
+        <img src="./images/template03/award5.png" alt="" />
+        <h3>Prêmio de Melhor Escritório de Advocacia</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        </div>
+
+        <div className='awards-block'>
+        <img src="./images/template03/award5.png" alt="" />
+        <h3>Prêmio de Melhor Escritório de Advocacia</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        </div>
+
+        <div className='awards-block'>
+        <img src="./images/template03/award5.png" alt="" />
+        <h3>Prêmio de Melhor Escritório de Advocacia</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        </div>
+
+        </AwardsRow1>
+
+      
+      <div className='awards-row2'>
+
+      </div>
+      </AwardsSection>
+
+      <PraticaceSection>
+
+      <LeftAwardBlock>
+
+      <LineAwardsSection></LineAwardsSection>
+        <h2>Nossas Áreas de Atuação</h2>
+        <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque.</p>
+        <img src="./images/template03/practiceareas1.jpg" alt="" />
+
+      </LeftAwardBlock>
+        
+      <RigthAwardBlock>
+      <div className='Areas-block'>
+          <h3>Separação e Divórcio</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        </div>
+        
+        <div className='Areas-block'>
+          <h3>Custódia da criança</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        </div>
+        
+        <div className='Areas-block'>
+          <h3>Pensão alimentícia</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        </div>
+        
+        <div className='Areas-block'>
+          <h3>Apoio do cônjuge</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        </div>
+        
+      </RigthAwardBlock>
+
+      <RigthAwardBlock>
+      <div className='Areas-block'>
+          <h3>Assuntos Internacionais</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        </div>
+        
+        <div className='Areas-block'>
+          <h3>Apelações de direito de família</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        </div>
+        
+        <div className='Areas-block'>
+          <h3>Proteção de patrimônio</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        </div>
+        
+        <div className='Areas-block'>
+          <h3>Serviços de alto patrimônio líquido</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+        </div>
+        
+      </RigthAwardBlock>
+      </PraticaceSection>
+
+      <HowWorksSection>
+        <LineAwardsSection></LineAwardsSection>
+      <HowWorksHeaderSection>
+
+        <div className="Header-How-Work">
+          <h2>Como trabalhamos</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.</p>
+        </div>
+
+      </HowWorksHeaderSection>
+      </HowWorksSection>
+
     </Container>
   );
 }
--- src/components/Templates/Template03/styles.tsx
+++ src/components/Templates/Template03/styles.tsx
@@ -225,4 +225,217 @@ export const LineAboutSection = styled.div`
   z-index: 999;
 `;
 
-export const IconSection = styled.section``;
+export const IconSection = styled.section`
+width: 100%;
+.Container-blocks{
+  padding-block: 5rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 5rem;
+  background-color: ${(props: ThemeProps) =>
+        mix(0.8, '#d4d2d21c', props.theme.headerFooterBgColor)};
+  }
+  .meta-icons{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 10px;
+    align-items: center;
+
+    h3{
+      font-size: 27px;
+      color: ${(props: ThemeProps) =>
+        mix(0.8, '#252424c5', props.theme.headerFooterBgColor)};
+      text-align: center;
+    }
+
+    p {
+      color: ${(props: ThemeProps) =>
+        mix(0.8, '#555555ca', props.theme.headerFooterBgColor)};
+      font-weight: 400;
+      line-height: 1.5rem;
+    }
+  }
+  svg {
+      fill: ${(props: ThemeProps) => props.theme.fontTitleColor};
+    }
+  `;
+
+export const AwardsSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  padding-block: 100px;
+  padding-left: 6rem;
+  padding-right: 8.2rem;
+`;
+
+export const HeaderAwards = styled.section`
+display: flex;
+flex-direction: column;
+gap: 0.1rem;
+  .header-awards{
+
+    h2,
+    h3 {
+      color: ${(props: ThemeProps) =>
+        mix(0.8, '#000', props.theme.headerFooterBgColor)};
+    }
+
+    h2 {
+      font-weight: 500;
+      font-size: 2.4rem;
+      margin-block: 20px;
+    }
+
+    h3 {
+      font-size: 1.2rem;
+      font-weight: 500;
+    }
+    p {
+    color: ${(props: ThemeProps) =>
+      mix(0.8, '#555555ca', props.theme.headerFooterBgColor)};
+    width:45%;
+    font-weight: 400;
+    line-height: 1.5rem;
+    }
+  }
+
+`;
+
+export const AwardsRow1 = styled.section`
+margin-top: 30px;
+display: flex;
+flex-direction: row;
+gap: 1rem;
+justify-content: center;
+
+
+.awards-block{
+  display:flex;
+  flex-direction: column;
+  margin-right: 20px;
+  background-color: ${(props: ThemeProps) =>
+      mix( 0.95, '#d9d9d912', props.theme.headerFooterBgColor)};
+  border-radius: 15px;
+  gap: 1rem;
+  padding: 20px;
+  box-shadow: 0 25px 80px -10px rgba(23.469701086956523,24.000447704985824,25.5,.15);
+
+  img{
+      width: 60px;
+      height: auto;
+    }
+
+    h2,
+    h3 {
+      color: ${(props: ThemeProps) =>
+        mix(0.8, '#000', props.theme.headerFooterBgColor)};
+    }
+
+    h2 {
+      font-weight: 500;
+      font-size: 2.4rem;
+      margin-block: 20px;
+    }
+
+    h3 {
+      font-size: 1.2rem;
+      font-weight: 500;
+    }
+
+    p {
+    color: ${(props: ThemeProps) =>
+      mix(0.8, '#555555ca', props.theme.headerFooterBgColor)};
+    width:80%;
+    font-weight: 400;
+    line-height: 1.5rem;
+  }
+}
+`;
+
+export const LineAwardsSection = styled.div`
+  height: 5.5px;
+  width: 4.5rem;
+  background-color: ${(props: ThemeProps) => props.theme.fontTitleColor};
+  border: none;
+  z-index: 999;
+`;
+
+export const PraticaceSection = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: ${(props: ThemeProps) =>
+      mix( 0.95, '#d9d9d912', props.theme.headerFooterBgColor)};;
+  padding-block: 100px;
+  padding-left: 6rem;
+  padding-right: 8.2rem;
+`;
+
+export const LeftAwardBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+  gap: 0.5rem;
+
+  img{
+    margin-top: 30px;
+    width: 507px;
+    height: 338px;
+    object-fit: cover;
+    box-shadow: 40px 40px 0 0 ${(props: ThemeProps) => mix(0.1, '#ffffffc6', props.theme.headerFooterBgColor)};
+  }
+
+  p {
+    color: #3a3a3a;
+    font-weight: 400;
+    line-height: 1.5rem;
+    width: 90%;
+  }
+`;
+
+export const RigthAwardBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 30%;
+  gap: 2.5rem;
+  
+  .Areas-block{ 
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+
+    h2 {
+      font-weight: 500;
+      font-size: 2.4rem;
+      margin-block: 20px;
+    }
+
+    h3 {
+      font-size: 1.2rem;
+      font-weight: 500;
+    }
+
+    p {
+    color: #3a3a3a;
+    font-weight: 400;
+    line-height: 1.5rem;
+    width: 90%;
+  }
+  }
+`;
+
+export const HowWorksSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
+export const HowWorksHeaderSection = styled.div`
+
+`;
